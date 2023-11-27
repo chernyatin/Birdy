620fc53c38713182461476bac8abb3a541b58701
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-11-27 16:34:49 +0300

    Initial Commit

---
+++ Birdy/BirdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdyApp.swift
+//  Birdy
+//
+//  Created by Денис Чернятин on 2023.11.27.
+//
+
+import SwiftUI
+
+@main
+struct BirdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
